88646d3d766b633160d629b979ab04db0ba7b4c2
luongtranduc <42030413+luongtranduc@users.noreply.github.com>
2020-01-01 17:30:16 +0700

    lalala

---
+++ Add.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="modal" :class="openmodal">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Register System</p>
+        <button class="delete" aria-label="close" @click="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input class="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">HTTP Code</label>
+          <div class="control is-expanded">
+            <div class="select is-fullwidth">
+              <select name='httpcode'>
+                <option>500</option>
+                <option>400</option>
+                <option>200</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success">Register</button>
+        <button class="button" @click="close">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["openmodal"],
+  methods: {
+    close() {
+      this.$emit("closeRequest");
+    }
+  }
+};
+</script>
---
+++ Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="notification">
+        <!-- Main container -->
+        <nav class="level">
+          <!-- Left side -->
+          <div class="level-left">
+            <div class="level-item">
+              <div class="field has-addons">
+                <p class="control">
+                  <input class="input" type="text" placeholder="Find a post" />
+                </p>
+                <p class="control">
+                  <button class="button">Search</button>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Right side -->
+          <div class="level-right">
+            <p class="level-item">
+              <strong>All</strong>
+            </p>
+            <p class="level-item">
+              <a>Published</a>
+            </p>
+            <p class="level-item">
+              <a>Drafts</a>
+            </p>
+            <p class="level-item">
+              <a>Deleted</a>
+            </p>
+            <p class="level-item">
+              <button class="button is-primary is-outlined" @click="openAdd">
+                Register
+              </button>
+            </p>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <Add :openmodal='addActive' @closeRequest='close'></Add>
+  </div>
+</template>
+<script>
+import Add from "./views/Add";
+export default {
+  components: {
+    Add
+  },
+  data(){
+    return{
+      addActive :''
+    }
+  },
+  methods:{
+    openAdd(){
+      this.addActive ='is-active';
+    },
+    close(){
+      this.addActive='';
+    }
+  }
+};
+</script>
